764d4fcc546f8326af2c334dee387137a041f589
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-05-11 08:44:54 +0300

    333

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Let;s go!</Text>
-      <Image />
+      <Text>Let;s Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
